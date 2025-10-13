a3597a932599f8d853a0599f0230165f02a7782d
xhonatankonomi <xhoni8962@gmail.com>
2025-10-13 14:42:49 +0200

    feat: Implement time-based greetings function
    This commit introduces a `greeting` function that returns
    different greetings (buongiorno, buon pomeriggio, or
    buonasera) based on the current time of day. The function
    is called with the user's name and the result is logged to
    the console.

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -8,24 +8,28 @@ const word = 'javascript';
 function countVowels(string) {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let count = 0;
+    let vowelsArray = [];
     for (let i = 0; i < string.length; i++) {
         if (vowels.includes(string[i])) {
+            vowelsArray.push(string[i]);
             count++;
         }
     }
-    return count;
+    return count + ' ' + `(${vowelsArray})`;
 } 
 
 const countVowels = (string) => {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let count = 0;
+    let vowelsArray = [];
     for (let i = 0; i < string.length; i++) {
         if (vowels.includes(string[i])) {
+            vowelsArray.push(string[i]);
             count++;
         }
     }
-    return count;
-} 
+    return count + ' ' + `(${vowelsArray})`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,21 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function greeting(name) {
+    let time = new Date().getHours();
+    if (time >= 0 && time < 13) {
+        return `buongiorno ${name}`;
+    } else if (time >= 13 && time < 17) {
+        return `buon pomeriggio ${name}`;
+    } else {
+        return `buonasera ${name}`;
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const greetingUser = greeting(name);
+console.log(greetingUser);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.

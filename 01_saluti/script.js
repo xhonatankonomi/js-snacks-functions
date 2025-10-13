9e60356df533551046d08f8940134be2aacc9740
xhonatankonomi <xhoni8962@gmail.com>
2025-10-13 14:18:17 +0200

    feat: Implement saluto function and log greeting
    This commit introduces the `saluto` function to generate personalized greetings.
    It initializes a constant `salutoUmano` with a greeting using the `saluto` function and logs it to the console.

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -2,12 +2,21 @@
 
 const userName = 'Mario';
 
-
 // Dichiara la funzione qui.
+
+/* function saluto(name) {
+    return `ciao ${name}`
+} */
+
+const saluto = (name) => {
+    return `ciao ${name}`
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const salutoUmano = saluto(userName);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+console.log(salutoUmano);

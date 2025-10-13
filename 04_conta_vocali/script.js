8e7a4feaacdc52f72e86b4b35b88bb9203a727ce
xhonatankonomi <xhoni8962@gmail.com>
2025-10-13 14:34:23 +0200

    feat: Add function to count vowels in a string
    This commit introduces the `countVowels` function, which iterates through a string to count the number of vowels. The function is invoked with the word 'javascript' and the resulting vowel count is logged to the console.

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,21 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function countVowels(string) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            count++;
+        }
+    }
+    return count;
+} 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const numberVowels = countVowels(word);
+console.log(numberVowels);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
